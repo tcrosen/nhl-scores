5f8622588609f2c0aa180030f5cfa58a915f70b3
Terry Rosen <tcrosen@gmail.com>
2015-11-07 20:30:42 -0500

    Get rid of a bunch of bloat

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -16,7 +16,7 @@ class Navigation extends Component {
   render() {
     return (
       <div className={classNames(this.props.className, 'Navigation')} role="navigation">
-        <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
+        <a className="Navigation-link" href="/scores" onClick={Link.handleClick}>Scores</a>
         <a className="Navigation-link" href="/contact" onClick={Link.handleClick}>Contact</a>
         <span className="Navigation-spacer"> | </span>
         <a className="Navigation-link" href="/login" onClick={Link.handleClick}>Log in</a>
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,6 @@ import http from './core/HttpClient';
 import App from './components/App';
 import ContentPage from './components/ContentPage';
 import ScoresPage from './components/ScoresPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
 
@@ -17,12 +15,7 @@ const router = new Router(on => {
     return component && <App context={state.context}>{component}</App>;
   });
 
-  on('/contact', async () => <ContactPage />);
-
-  on('/login', async () => <LoginPage />);
-
-  on('/register', async () => <RegisterPage />);
-
+  on('/', async () => <ScoresPage />);
   on('/scores', async () => <ScoresPage />);
 
   on('*', async (state) => {
