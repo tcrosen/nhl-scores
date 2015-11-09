342d33c71c1c7ef8fc7f7517fbd7ecf2f4a96e18
Terry Rosen <tcrosen@gmail.com>
2015-11-08 21:43:10 -0500

    gamestore update

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -1,10 +1,12 @@
 import request from 'superagent';
 import jsonp from 'superagent-jsonp';
 
+const KIMONO_API_KEY = 'abe6b22285a4d123b8d3ed875ac78331';
+
 export default {
   getAll(done) {
     request
-      .get('https://www.kimonolabs.com/api/3q4kjoay?apikey=abe6b22285a4d123b8d3ed875ac78331')
+      .get(`https://www.kimonolabs.com/api/3q4kjoay?apikey=${KIMONO_API_KEY}`)
       .use(jsonp)
       .end(done);
   },
