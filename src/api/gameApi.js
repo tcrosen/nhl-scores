5326406920cbfe4e2b4cd371fca4515ae3775268
Terry Rosen <tcrosen@gmail.com>
2015-11-10 11:17:44 -0500

    change to on demand endpoint with date param

--- src/api/gameApi.js
+++ src/api/gameApi.js
@@ -6,11 +6,10 @@ import { map } from 'lodash';
 export default {
   getAll(done) {
     request
-      .get(`https://www.kimonolabs.com/api/3q4kjoay?apikey=${KIMONO_API_KEY}`)
+      .get(`https://www.kimonolabs.com/api/ondemand/3q4kjoay?apikey=${KIMONO_API_KEY}&date=11/10/2015`)
       .use(jsonp)
       .end((err, resp) => {
         done(map(resp.body.results.games, (game) => {
-          game.homeTeamScore = Math.random(1, 10);
           return game;
         }));
       });
